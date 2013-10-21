582d2ae3d6f39f1693e070c3b2db233f0d43a4e0
Pepe García <jl.garhdez@gmail.com>
2013-10-22 00:14:11 +0200

    first commit

---
+++ test/clojummendations/test/handler.clj
@@ -0,0 +1,14 @@
+(ns clojummendations.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        clojummendations.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
