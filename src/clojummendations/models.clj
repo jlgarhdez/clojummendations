8a3c49421932694f126dfa102f31c5736b2a2023
Pepe García <jl.garhdez@gmail.com>
2013-10-24 02:19:10 +0200

    Solved a bug whereby searching for artists not listed in Lastfm.com breaks the app.

--- src/clojummendations/models.clj
+++ src/clojummendations/models.clj
@@ -22,16 +22,18 @@
   "Calls the API and retrieves a JSON of artists related to the given artist"
   [artist-name]
   (let [json (get-json (construct-url artist-name))]
-    ((try 
+    (if (not (string? (get (get json "similarartists") "artist"))) ; Check if the query returned something
       (map
         (fn [artist] ; This function returns the image, url and name of each artist
           {:name (get artist "name")
            :url (str "http://" (get artist "url"))
            :image (((get artist "image") 3) "#text")})
         (get (get json "similarartists") "artist"))
-       (catch Exception e ("An error occurred"))))) ; Gets the array of similar artists
+      false))) ; Gets the array of similar artists
 
 (defn display-related-artists
   "returns the HTML for the related artists of a given artist"
   [artist artist-template]
-  (map (fn [a] (artist-template (get a :name) (get a :image) (get a :url))) (vec (get-related-artists artist))))
+  (if (false? (get-related-artists artist))
+    (str "Sorry, no related artist found for " artist)
+    (map (fn [a] (artist-template (get a :name) (get a :image) (get a :url))) (vec (get-related-artists artist)))))
